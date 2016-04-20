4eb2ff5590ff26ce7d5d4d9ca85007484ec36ee4
filipemleite94 <filipemleite94@gmail.com>
2016-04-20 05:51:48 -0300

    Atualização
    Adicionado comentários a subclasse triângulo

--- triangulo.java
+++ triangulo.java
@@ -1,15 +1,21 @@
-
+//Subclasse
 public class triangulo extends shape{
 	double l1, l2, l3, p;
+	
+	//construtor chama o contrutor da superclasse
 	public triangulo(String nome) {
 		super(nome);
 	}
+	
+	//recebe dados particulares a ela
 	public void definir(double l1, double l2, double l3){
 		this.l1=l1;
 		this.l2=l2;
 		this.l3=l3;
 		p=(l1+l2+l3)/2;
 	}
+	
+	//tipo de calculo especifico ao triangulo
 	public double Calculararea(){
 		return Math.sqrt(p*(p-l1)*(p-l2)*(p-l3));
 	}
